c846aeed9db8080aec7e65aab0e6d8a59a29e0bb
Fernando Polanco <A24216406@alumnos.uady.mx>
2026-09-21 10:53:33 -0600

    refactor: change id type from String to int

--- src/domain/model/Order.java
+++ src/domain/model/Order.java
@@ -4,16 +4,20 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Order {
-    private final String id;
+    private final int id;
     private final String customerName;
     private final List<OrderItem> products;
     private OrderStatus status;
 
-    public Order(String id, String customerName, List<OrderItem> products) {
+    public Order(int id, String customerName, List<OrderItem> products, OrderStatus status) {
         this.id = id;
         this.customerName = customerName;
         this.products = products;
-        this.status = OrderStatus.PENDING;
+        this.status = status;
+    }
+
+    public static Order create(int id, String customerName, List<OrderItem> products) {
+        return new Order(id, customerName, products, OrderStatus.PENDING);
     }
 
     public BigDecimal subtotal() {
@@ -24,5 +28,21 @@ public class Order {
         return products.stream()
                 .map(OrderItem::subtotal)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public List<OrderItem> getProducts() {
+        return List.copyOf(products);
+    }
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+    public OrderStatus getStatus() {
+        return status;
     }
 }
--- src/domain/model/Product.java
+++ src/domain/model/Product.java
@@ -5,12 +5,12 @@ import domain.exception.InsufficientStockException;
 import java.math.BigDecimal;
 
 public class Product {
-    private final String id;
+    private final int id;
     private final String name;
     private final BigDecimal price;
     private int stock;
 
-    public Product(String id, String name, BigDecimal price, int stock) {
+    public Product(int id, String name, BigDecimal price, int stock) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -34,7 +34,7 @@ public class Product {
         this.stock -= quantity;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
     public String getName() {
