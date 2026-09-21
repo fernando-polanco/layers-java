d42331d941045c483f2f7292d922dbc576ab1d6b
Fernando Polanco <A24216406@alumnos.uady.mx>
2026-09-21 10:59:00 -0600

    feat: add interface that defines repository methods for order management

---
+++ src/domain/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package domain.repository;
+
+import domain.model.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository {
+    Order save(Order order);
+    Optional<Order> findById(String id);
+    List<Order> findAll();
+}
