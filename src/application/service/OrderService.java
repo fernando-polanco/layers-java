e8298f6a2aa44931481e5b06a28383f37baf9664
Fernando Polanco <A24216406@alumnos.uady.mx>
2026-09-22 10:57:26 -0600

    refactor: change getOrder method signature to use int instead of String

--- src/application/service/OrderService.java
+++ src/application/service/OrderService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface OrderService {
     OrderDTO createOrder(CreateOrderRequest request);
-    OrderDTO getOrder(String id);
+    OrderDTO getOrder(int id);
     List<OrderDTO> listOrders();
 
 }
